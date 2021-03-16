9a410a9d097562fcfc5c0350cbb2fa7afc45c8d8
Viranchee Lotia <viranchee@outlook.com>
2021-03-16 15:55:56 +0530

    Indentation

--- ios-template/Helpers/RWAutoLayout.swift
+++ ios-template/Helpers/RWAutoLayout.swift
@@ -32,84 +32,84 @@ import UIKit
 // swiftlint:disable function_parameter_count
 // MARK: NSLayoutConstraint Convenience methods
 extension NSLayoutConstraint {
-    
-    // Pins an attribute of a view to an attribute of another view
-    static func pinning(view: UIView, attribute: NSLayoutConstraint.Attribute, toView: UIView?, toAttribute: NSLayoutConstraint.Attribute, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: toView, attribute: toAttribute, multiplier: multiplier, constant: constant)
-    }
-    
-    // Pins an array of NSLayoutAttributes of a view to a specific view (has to respect view tree hierarchy)
-    static func pinning(view: UIView, toView: UIView?, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat, constant: CGFloat) -> [NSLayoutConstraint] {
-        return attributes.compactMap({ (attribute) -> NSLayoutConstraint in
-            return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: toView, attribute: attribute, multiplier: multiplier, constant: constant)
-        })
-    }
-    
-    // Pins bottom, top, leading and trailing of a view to a specific view (has to respect view tree hierarchy)
-    static func pinningEdges(view: UIView, toView: UIView?) -> [NSLayoutConstraint] {
-        let attributes: [NSLayoutConstraint.Attribute] = [.top, .bottom, .leading, .trailing]
-        return NSLayoutConstraint.pinning(view: view, toView: toView, attributes: attributes, multiplier: 1.0, constant: 1.0)
-    }
-    
-    // Pins bottom, top, leading and trailing of a view to its superview
-    static func pinningEdgesToSuperview(view: UIView) -> [NSLayoutConstraint] {
-        return NSLayoutConstraint.pinningEdges(view: view, toView: view.superview)
-    }
-    
-    // Pins specified attribute to superview with specified or default multiplier and constant
-    static func pinningToSuperview(view: UIView, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat, constant: CGFloat) -> [NSLayoutConstraint] {
-        return NSLayoutConstraint.pinning(view: view, toView: view.superview, attributes: attributes, multiplier: multiplier, constant: constant)
-    }
+  
+  // Pins an attribute of a view to an attribute of another view
+  static func pinning(view: UIView, attribute: NSLayoutConstraint.Attribute, toView: UIView?, toAttribute: NSLayoutConstraint.Attribute, multiplier: CGFloat, constant: CGFloat) -> NSLayoutConstraint {
+    return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: toView, attribute: toAttribute, multiplier: multiplier, constant: constant)
+  }
+  
+  // Pins an array of NSLayoutAttributes of a view to a specific view (has to respect view tree hierarchy)
+  static func pinning(view: UIView, toView: UIView?, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat, constant: CGFloat) -> [NSLayoutConstraint] {
+    return attributes.compactMap({ (attribute) -> NSLayoutConstraint in
+      return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: .equal, toItem: toView, attribute: attribute, multiplier: multiplier, constant: constant)
+    })
+  }
+  
+  // Pins bottom, top, leading and trailing of a view to a specific view (has to respect view tree hierarchy)
+  static func pinningEdges(view: UIView, toView: UIView?) -> [NSLayoutConstraint] {
+    let attributes: [NSLayoutConstraint.Attribute] = [.top, .bottom, .leading, .trailing]
+    return NSLayoutConstraint.pinning(view: view, toView: toView, attributes: attributes, multiplier: 1.0, constant: 1.0)
+  }
+  
+  // Pins bottom, top, leading and trailing of a view to its superview
+  static func pinningEdgesToSuperview(view: UIView) -> [NSLayoutConstraint] {
+    return NSLayoutConstraint.pinningEdges(view: view, toView: view.superview)
+  }
+  
+  // Pins specified attribute to superview with specified or default multiplier and constant
+  static func pinningToSuperview(view: UIView, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat, constant: CGFloat) -> [NSLayoutConstraint] {
+    return NSLayoutConstraint.pinning(view: view, toView: view.superview, attributes: attributes, multiplier: multiplier, constant: constant)
+  }
 }
 
 // MARK: UIView Convenience methods
 extension UIView {
-    
-    /// Pin Edges of View to it's Superview
-    func pinEdgesToSuperview() {
-        translatesAutoresizingMaskIntoConstraints = false
-        guard let superview = self.superview else { return }
-        let attributes: [NSLayoutConstraint.Attribute] = [.top, .bottom, .leading, .trailing]
-        let constraints = NSLayoutConstraint.pinningToSuperview(view: self, attributes: attributes, multiplier: 1, constant: 0)
-        superview.addConstraints(constraints)
-    }
-    
-    /// Pin certain attributes to superView
-    /// - Parameters:
-    ///   - attributes: Layout Attributes
-    ///   - multiplier: Multiplier
-    ///   - constant: Constant
-    func pinToSuperview(forAtrributes attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
-        translatesAutoresizingMaskIntoConstraints = false
-        guard let superview = self.superview else { return }
-        let constraints = NSLayoutConstraint.pinningToSuperview(view: self, attributes: attributes, multiplier: multiplier, constant: constant)
-        superview.addConstraints(constraints)
-    }
-    
-    /// Pin certain layout properties to particular view
-    /// - Parameters:
-    ///   - toView: The View to pin to
-    ///   - attributes: Attributes
-    ///   - multiplier: Multiplier
-    ///   - constant: Constant
-    func pin(toView: UIView, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
-        translatesAutoresizingMaskIntoConstraints = false
-        guard let superview = self.superview else { return }
-        let constraints = NSLayoutConstraint.pinning(view: self, toView: toView, attributes: attributes, multiplier: multiplier, constant: constant)
-        superview.addConstraints(constraints)
-    }
-    
-    /// Pin certain attributes to a View
-    /// - Parameters:
-    ///   - attribute: Attributes
-    ///   - toView: View to pin to
-    ///   - toAttribute: that View's attribute
-    ///   - multiplier: Multiplier
-    ///   - constant: Constant
-    func pin(attribute: NSLayoutConstraint.Attribute, toView: UIView?, toAttribute: NSLayoutConstraint.Attribute, multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
-        translatesAutoresizingMaskIntoConstraints = false
-        guard let superview = self.superview else { return }
-        let constraint = NSLayoutConstraint.pinning(view: self, attribute: attribute, toView: toView, toAttribute: toAttribute, multiplier: multiplier, constant: constant)
-        superview.addConstraint(constraint)
-    }
+  
+  /// Pin Edges of View to it's Superview
+  func pinEdgesToSuperview() {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let attributes: [NSLayoutConstraint.Attribute] = [.top, .bottom, .leading, .trailing]
+    let constraints = NSLayoutConstraint.pinningToSuperview(view: self, attributes: attributes, multiplier: 1, constant: 0)
+    superview.addConstraints(constraints)
+  }
+  
+  /// Pin certain attributes to superView
+  /// - Parameters:
+  ///   - attributes: Layout Attributes
+  ///   - multiplier: Multiplier
+  ///   - constant: Constant
+  func pinToSuperview(forAtrributes attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraints = NSLayoutConstraint.pinningToSuperview(view: self, attributes: attributes, multiplier: multiplier, constant: constant)
+    superview.addConstraints(constraints)
+  }
+  
+  /// Pin certain layout properties to particular view
+  /// - Parameters:
+  ///   - toView: The View to pin to
+  ///   - attributes: Attributes
+  ///   - multiplier: Multiplier
+  ///   - constant: Constant
+  func pin(toView: UIView, attributes: [NSLayoutConstraint.Attribute], multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraints = NSLayoutConstraint.pinning(view: self, toView: toView, attributes: attributes, multiplier: multiplier, constant: constant)
+    superview.addConstraints(constraints)
+  }
+  
+  /// Pin certain attributes to a View
+  /// - Parameters:
+  ///   - attribute: Attributes
+  ///   - toView: View to pin to
+  ///   - toAttribute: that View's attribute
+  ///   - multiplier: Multiplier
+  ///   - constant: Constant
+  func pin(attribute: NSLayoutConstraint.Attribute, toView: UIView?, toAttribute: NSLayoutConstraint.Attribute, multiplier: CGFloat = 1.0, constant: CGFloat = 0.0) {
+    translatesAutoresizingMaskIntoConstraints = false
+    guard let superview = self.superview else { return }
+    let constraint = NSLayoutConstraint.pinning(view: self, attribute: attribute, toView: toView, toAttribute: toAttribute, multiplier: multiplier, constant: constant)
+    superview.addConstraint(constraint)
+  }
 }
--- ios-template/Helpers/UITableView+Extension.swift
+++ ios-template/Helpers/UITableView+Extension.swift
@@ -29,67 +29,67 @@
 import UIKit
 
 extension UITableView {
-    
-    func registerCell<Cell: UITableViewCell>(_ cellClass: Cell.Type) {
-        let cellName = String(describing: cellClass)
-        register(cellClass, forCellReuseIdentifier: cellName)
+  
+  func registerCell<Cell: UITableViewCell>(_ cellClass: Cell.Type) {
+    let cellName = String(describing: cellClass)
+    register(cellClass, forCellReuseIdentifier: cellName)
+  }
+  
+  func registerNib<Cell: UITableViewCell>(_ cellClass: Cell.Type) {
+    let cellName = String(describing: cellClass)
+    register(UINib(nibName: cellName, bundle: nil), forCellReuseIdentifier: cellName)
+  }
+  
+  func dequeueReusableCell<Cell: UITableViewCell>(forIndexPath indexPath: IndexPath) -> Cell {
+    let identifier = String(describing: Cell.self)
+    guard let cell = dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? Cell else {
+      fatalError("Error for cell id: \(identifier) at \(indexPath)")
     }
-    
-    func registerNib<Cell: UITableViewCell>(_ cellClass: Cell.Type) {
-        let cellName = String(describing: cellClass)
-        register(UINib(nibName: cellName, bundle: nil), forCellReuseIdentifier: cellName)
+    return cell
+  }
+  
+  func registerHeader<Header: UITableViewHeaderFooterView>(_ headerClass: Header.Type) {
+    let headerName = String(describing: headerClass)
+    register(headerClass, forHeaderFooterViewReuseIdentifier: headerName)
+  }
+  
+  func registerHeaderNib<Header: UITableViewHeaderFooterView>(_ headerClass: Header.Type) {
+    let nibName = String(describing: headerClass)
+    let nib = UINib(nibName: nibName, bundle: nil)
+    register(nib, forHeaderFooterViewReuseIdentifier: nibName)
+  }
+  
+  func dequeueReusableHeader<Header: UITableViewHeaderFooterView>() -> Header {
+    let identifier = String(describing: Header.self)
+    guard let header = dequeueReusableHeaderFooterView(withIdentifier: identifier) as? Header else {
+      fatalError("Error for header id: \(identifier)")
     }
+    return header
     
-    func dequeueReusableCell<Cell: UITableViewCell>(forIndexPath indexPath: IndexPath) -> Cell {
-        let identifier = String(describing: Cell.self)
-        guard let cell = dequeueReusableCell(withIdentifier: identifier, for: indexPath) as? Cell else {
-            fatalError("Error for cell id: \(identifier) at \(indexPath)")
-        }
-        return cell
-    }
-    
-    func registerHeader<Header: UITableViewHeaderFooterView>(_ headerClass: Header.Type) {
-        let headerName = String(describing: headerClass)
-        register(headerClass, forHeaderFooterViewReuseIdentifier: headerName)
-    }
-    
-    func registerHeaderNib<Header: UITableViewHeaderFooterView>(_ headerClass: Header.Type) {
-        let nibName = String(describing: headerClass)
-        let nib = UINib(nibName: nibName, bundle: nil)
-        register(nib, forHeaderFooterViewReuseIdentifier: nibName)
-    }
-    
-    func dequeueReusableHeader<Header: UITableViewHeaderFooterView>() -> Header {
-        let identifier = String(describing: Header.self)
-        guard let header = dequeueReusableHeaderFooterView(withIdentifier: identifier) as? Header else {
-            fatalError("Error for header id: \(identifier)")
-        }
-        return header
-        
-    }
-    
+  }
+  
 }
 
 class GenericTableViewCell<View: UIView>: UITableViewCell {
-    var cellView: View? {
-        didSet {
-            guard cellView != nil else { return }
-            setupViews()
-        }
+  var cellView: View? {
+    didSet {
+      guard cellView != nil else { return }
+      setupViews()
     }
+  }
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    selectionStyle = .none
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setupViews() {
+    guard let cellView = cellView else { return }
+    addSubview(cellView)
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .default, reuseIdentifier: reuseIdentifier)
-        selectionStyle = .none
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupViews() {
-        guard let cellView = cellView else { return }
-        addSubview(cellView)
-        
-    }
+  }
 }
