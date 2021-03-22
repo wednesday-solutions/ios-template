9a894a19f0e7329614966c798854a519e5b8f870
Viranchee Lotia <viranchee@outlook.com>
2021-03-22 17:29:38 +0530

    Add UnitTests for ViewModel

--- ios-templateTests/ios_templateTests.swift
+++ ios-templateTests/ios_templateTests.swift
@@ -9,25 +9,70 @@ import XCTest
 @testable import ios_template
 
 class ios_templateTests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  
+  /// This test checks if github user Viranchee is searchable. It tests Networking logic as well
+  /// - Throws: User "Viranchee" not found on github
+  func testUserVirancheeIsThere() throws {
+    let expectation = XCTestExpectation()
+    let waiter = XCTWaiter()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let userSearchViewModel = UserSearchViewModel(searchText: "viranchee")
+    userSearchViewModel.searchUsers {
+      expectation.fulfill()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    waiter.wait(for: [expectation], timeout: 2)
+    
+    XCTAssert(userSearchViewModel.userList.contains { (user) -> Bool in
+      return user.login == "Viranchee"
+    })
+  }
+  
+  /// This test checks if github user Viranchee is searchable. It tests Networking logic as well
+  /// - Throws: User "Viranchee" not found on github
+  func testRandomUserDoesNotExist() throws {
+    let expectation = XCTestExpectation()
+    let waiter = XCTWaiter()
+
+    let userSearchViewModel = UserSearchViewModel(searchText: "wjefisdlkjavjdvnsdvjsiojvlkvaorgjskvasv")
+    userSearchViewModel.searchUsers {
+      expectation.fulfill()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    waiter.wait(for: [expectation], timeout: 2)
+    XCTAssert(userSearchViewModel.userList.count == 0)
+  }
+  
+  func testRepoListViewModelForUserViranchee() {
+    let expectation = XCTestExpectation()
+    let waiter = XCTWaiter()
+
+    let repoListViewModel = RepoListViewModel(user: "Viranchee")
+    repoListViewModel.getRepos {
+      expectation.fulfill()
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    waiter.wait(for: [expectation], timeout: 2)
+    dump(repoListViewModel.repositories)
+    XCTAssert(repoListViewModel.repositories.contains(where: { (repo) -> Bool in
+      repo.name == "Resume"
+    }))
+  }
+  
+  
+  func testPerformanceExample() throws {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-
+  }
+  
 }
