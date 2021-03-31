c0f2e26b930184c00d5d8f94f50eadd10655ed37
Viranchee Lotia <viranchee@outlook.com>
2021-03-31 16:16:35 +0530

    Updating Tests. Note, these tests fail, need to improve testing strategy

--- ios-templateTests/HomeViewModelTests.swift
+++ ios-templateTests/HomeViewModelTests.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import iOS_Template
 
 class HomeViewModelTests: XCTestCase {
+
   override func setUpWithError() throws {
     
   }
@@ -19,7 +20,22 @@ class HomeViewModelTests: XCTestCase {
   
   func testSearchingUser() {
     let expectation = XCTestExpectation()
-    let waiter = XCTWaiter()
+    let viewModel = HomeViewModel()
+
+    XCTAssert(viewModel.model.count == 0)
+    viewModel.onDataLoad = { () -> Void in
+      expectation.fulfill()
+    }
+    viewModel.searchStringChanged(newString: "Viranchee")
+    
+    wait(for: [expectation], timeout: 2)
+    
+    XCTAssert(viewModel.model.count > 1)
+
+  }
+  
+  func testSearchingUserModifiedString() {
+    let expectation = XCTestExpectation()
     let viewModel = HomeViewModel()
     
     XCTAssert(viewModel.model.count == 0)
@@ -28,13 +44,12 @@ class HomeViewModelTests: XCTestCase {
     }
     
     viewModel.searchStringChanged(newString: "Viranchee")
-    waiter.wait(for: [expectation], timeout: 5)
-    XCTAssert(viewModel.model.count > 1)
     
     viewModel.searchStringChanged(newString: "V")
-    waiter.wait(for: [expectation], timeout: 5)
+    wait(for: [expectation], timeout: 2)
     XCTAssert(viewModel.model.count > 3)
 
   }
+  
   
 }
--- ios-templateTests/RepoListViewModelTests.swift
+++ ios-templateTests/RepoListViewModelTests.swift
@@ -19,7 +19,6 @@ class RepoListModelTests: XCTestCase {
   
   func testSearchingUserRepositories() {
     let expectation = XCTestExpectation()
-    let waiter = XCTWaiter()
     let viewModel = RepoListViewModel(user: "Viranchee")
     
     XCTAssert(viewModel.repositories.count == 0)
@@ -28,15 +27,33 @@ class RepoListModelTests: XCTestCase {
     }
     
     viewModel.searchForUserRepositories()
-    waiter.wait(for: [expectation], timeout: 5)
+    wait(for: [expectation], timeout: 2)
     XCTAssert(viewModel.repositories.count > 1)
     
     let reposCount = viewModel.repositories.count
     
     viewModel.endOfPageReached()
-    waiter.wait(for: [expectation], timeout: 5)
+    wait(for: [expectation], timeout: 2)
     XCTAssert(viewModel.repositories.count > reposCount)
 
   }
   
+  func testLoadingMoreRepos() {
+    let expectation = XCTestExpectation()
+    let viewModel = RepoListViewModel(user: "Viranchee")
+    
+    XCTAssert(viewModel.repositories.count == 0)
+    viewModel.onDataLoad = { () -> Void in
+      expectation.fulfill()
+    }
+    
+    viewModel.searchForUserRepositories()
+    wait(for: [], timeout: 2)
+    let reposCount = viewModel.repositories.count
+    
+    viewModel.endOfPageReached()
+    wait(for: [expectation], timeout: 2)
+    XCTAssert(viewModel.repositories.count > reposCount)
+
+  }
 }
