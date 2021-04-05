ab768dc4eae2ea08f5537174f67dae5f122b343b
Viranchee Lotia <viranchee@outlook.com>
2021-04-05 19:09:22 +0530

    Add fastlane lanes

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -9,8 +9,54 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
-	}
+  func customLane() {
+    desc("Description of what the lane does")
+    // add actions here: https://docs.fastlane.tools/actions
+  }
+  
+  func swiftLintLane() {
+    desc("Run SwiftLint")
+    swiftlint(configFile: ".swiftlint.yml",
+              strict: true,
+              ignoreExitStatus: false,
+              raiseIfSwiftlintError: true,
+              executable: "Pods/SwiftLint/swiftlint"
+    )
+  }
+  
+  func buildLane() {
+    desc("Build for testing")
+    scan(workspace: "Portfolius-iOS.xcworkspace",
+         derivedDataPath: "derivedData",
+         buildForTesting: true,
+         xcargs: "CI=true")
+  }
+  
+  func unitTestLane() {
+    desc("Run unit tests")
+    scan(workspace: "Portfolius-iOS.xcworkspace",
+         onlyTesting: ["PortfoliusCoreTests"],
+         derivedDataPath: "derivedData",
+         testWithoutBuilding: true)
+  }
+  
+  func integrationTestLane() {
+    desc("Run integration tests")
+    scan(workspace: "Portfolius-iOS.xcworkspace",
+         onlyTesting: ["Portfolius-iOSTests"],
+         derivedDataPath: "derivedData",
+         testWithoutBuilding: true)
+  }
+  
+  func uiTestLane() {
+    desc("Run UI tests")
+    scan(workspace: "Portfolius-iOS.xcworkspace",
+         onlyTesting: "Portfolius-iOSUITests",
+         derivedDataPath: "derivedData",
+         testWithoutBuilding: true)
+  }
+  
+  
+  
+  
 }
