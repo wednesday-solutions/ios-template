901422a820e557844f59575e415de413b44f8f49
Prathamesh Kowarkar <prathamesh@wednesday.is>
2021-05-28 15:40:16 +0530

    Add URLProvider for API URL generation

---
+++ iOS-Template/Networking/URLProvider.swift
@@ -0,0 +1,29 @@
+//
+//  URLProvider.swift
+//  iOS-Template
+//
+//  Created by apple on 28/05/21.
+//
+
+import Foundation
+
+struct URLProvider {
+  
+  private let components: URLComponents = {
+    var components = URLComponents()
+    components.scheme = "https"
+    components.host = "api.github.com"
+    return components
+  }()
+  
+  func makeSearchURL(query: String, page: Int) -> URL? {
+    var components = self.components
+    components.path = "/search/users"
+    components.queryItems = [
+      URLQueryItem(name: "q", value: "\"\(query)\""),
+      URLQueryItem(name: "page", value: String(describing: page))
+    ]
+    return components.url
+  }
+  
+}
