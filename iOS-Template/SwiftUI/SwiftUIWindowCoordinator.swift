bd41e6d4aa7f97e2448786809870eef3fcc7a607
Prathamesh Kowarkar <prathamesh@wednesday.is>
2021-05-31 15:22:46 +0530

    Add SwiftUI window coordinator

---
+++ iOS-Template/SwiftUI/SwiftUIWindowCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftUIWindowCoordinator.swift
+//  iOS-Template
+//
+//  Created by apple on 31/05/21.
+//
+
+import SwiftUI
+
+class SwiftUIWindowCoordinator: NSObject, WindowCoordinator {
+  
+  private(set) var window: UIWindow
+  private(set) var childCoordinators: [Coordinator] = []
+  private let environment: Environment
+  
+  init(window: UIWindow, environment: Environment) {
+    self.window = window
+    self.environment = environment
+    super.init()
+  }
+  
+  convenience init?(scene: UIScene, environment: Environment) {
+    guard let windowScene = scene as? UIWindowScene else { return nil }
+    self.init(window: UIWindow(windowScene: windowScene), environment: environment)
+  }
+  
+  func start() {
+    let view = makeView()
+    window.rootViewController = UIHostingController(rootView: view)
+    window.makeKeyAndVisible()
+  }
+  
+  func makeView() -> some View {
+    Text("Hello, World!")
+  }
+  
+}
