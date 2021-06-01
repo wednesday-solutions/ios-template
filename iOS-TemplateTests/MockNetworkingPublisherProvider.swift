68cf25b975c39bb4bd162df8f109573fa09b3cf0
Prathamesh Kowarkar <prathamesh@wednesday.is>
2021-06-01 13:40:15 +0530

    Add mock networking publisher provider

---
+++ iOS-TemplateTests/MockNetworkingPublisherProvider.swift
@@ -0,0 +1,36 @@
+//
+//  MockNetworkingPublisherProvider.swift
+//  iOS-TemplateTests
+//
+//  Created by apple on 01/06/21.
+//
+
+import Combine
+import Foundation
+@testable import iOS_Template
+
+struct MockNetworkingPublisherProvider: NetworkingPublisherProviding {
+  
+  private let data: Data
+  private let response: URLResponse
+  
+  private var publisher: AnyPublisher<(data: Data, response: URLResponse), URLError> {
+    Result.success((data, response))
+      .publisher
+      .eraseToAnyPublisher()
+  }
+  
+  init(data: Data, response: HTTPURLResponse) {
+    self.data = data
+    self.response = response
+  }
+  
+  func publisher(for url: URL) -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
+    publisher
+  }
+  
+  func publisher(for request: URLRequest) -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
+    publisher
+  }
+  
+}
