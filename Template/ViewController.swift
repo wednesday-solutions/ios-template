b1ee2df8e230959dfeaedfc519cf0b204a50da0f
Rameez Khan <rameez@wednesday.is>
2021-10-14 14:33:37 +0530

    generating assets, colors and fonts using swiftgen

--- Template/ViewController.swift
+++ Template/ViewController.swift
@@ -11,16 +11,34 @@ class ViewController: UIViewController {
     let label: UILabel = {
         let label = UILabel()
         label.text = L10n.welcome
+        // Picking up color and font from generated files so no safe unwrapping during initialisation
+        label.backgroundColor = Asset.accentColor.color
+        label.font = FontFamily.Roboto.blackItalic.font(size: 24.0)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    let wednesdayImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        // Picking up images from generated files so no need for safe unwrapping
+        imageView.image = Asset.wednesday.image
+        return imageView
     }()
     
     override func loadView() {
         super.loadView()
         view.addSubview(label)
+        view.addSubview(wednesdayImageView)
         NSLayoutConstraint.activate([
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            // imageViewConstraints
+            wednesdayImageView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 16.0),
+            wednesdayImageView.heightAnchor.constraint(equalToConstant: 20.0),
+            wednesdayImageView.widthAnchor.constraint(equalToConstant: 20.0),
+            wednesdayImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     override func viewDidLoad() {
@@ -31,8 +49,7 @@ class ViewController: UIViewController {
         print(SecretConfig.apiSecretKey)
         
         // Test swiftLint
-        var r = "test"
-        print("r is", r)
+//        var r = "test"
+//        print("r is", r)
     }
 }
-
